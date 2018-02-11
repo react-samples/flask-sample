299d5356f3f11fc1ab02087b12b19d7dcc9b947e
example <root@example.com>
2018-02-11 16:30:55 +0900

    update

--- 4-1/app.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask, render_template
-
-app = Flask(__name__)
-
-@app.route('/', methods=['GET'])
-def index():
-    return render_template('index.html', seq=["hoge", "fuga", "foo"])
-
